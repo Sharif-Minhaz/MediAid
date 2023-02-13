0f1fcb5a8247378171b9a9f10246c94d313fe5bc
Minhaz <sharif35-3001@diu.edu.bd>
2023-02-14 03:47:17 +0600

    feat: reset-psw page added - client

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -3,12 +3,14 @@ import theme from "../theme/theme";
 import Login from "../features/auth/Login";
 import "./App.css";
 import Register from "../features/auth/Register";
+import ResetPassword from "../features/auth/ResetPassword"
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			{/* <Login /> */}
-			<Register />
+			<Login />
+			{/* <Register /> */}
+			{/* <ResetPassword /> */}
 		</ThemeProvider>
 	);
 }
---
+++ client/src/features/auth/ResetPassword.jsx
@@ -0,0 +1,171 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import KeyIcon from "@mui/icons-material/Key";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import {
+	Button,
+	CssBaseline,
+	FormControlLabel,
+	Checkbox,
+	Grid,
+	Box,
+	Typography,
+	Container,
+	Chip,
+	Divider,
+	FormControl,
+	InputLabel,
+	OutlinedInput,
+	Paper,
+	InputAdornment,
+	IconButton,
+} from "@mui/material";
+import { useTheme } from "@emotion/react";
+
+const ResetPassword = () => {
+	const theme = useTheme();
+	const [showPassword, setShowPassword] = useState(false);
+	const [matchedPrevPsw, setMatchedPrevPsw] = useState(false);
+
+	const handleClickShowPassword = () => setShowPassword((show) => !show);
+	const handleMouseDownPassword = (event) => {
+		event.preventDefault();
+	};
+
+	return (
+		<Container component="main" sx={{ width: "520px !important" }}>
+			<CssBaseline />
+			<Paper
+				sx={{
+					marginBlock: 8,
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					p: 3,
+				}}
+			>
+				<img src="/images/cover.png" height="85" />
+
+				<Typography fontWeight={700} color="primary" component="h5" variant="h5">
+					Reset Password
+				</Typography>
+				<Typography color="primary.contrastText" mt={2} mb="1px">
+					Enter your previous password & reset
+				</Typography>
+				<Divider sx={{ width: "100%", mt: "20px", mb: "20px" }}>
+					<Chip icon={<KeyIcon />} label={" Authentication"} variant="outlined" />
+				</Divider>
+				<Typography fontWeight={500} sx={{ fontSize: "0.875rem" }} mb={1}>
+					Reset with previous password
+				</Typography>
+				<Box component="form" noValidate sx={{ mt: 1 }}>
+					<FormControl
+						fullWidth
+						// error={Boolean(errors.email)}
+						// error={true}
+						sx={{ ...theme.customInput }}
+					>
+						<InputLabel htmlFor="outlined-adornment-password-login">
+							Previous Password
+						</InputLabel>
+						<OutlinedInput
+							id="outlined-adornment-password-login"
+							// value={"values.password"}
+							name="prevPassword"
+							// onBlur={"handleBlur"}
+							// onChange={"handleChange"}
+							label="Previous Password"
+							type={showPassword ? "text" : "password"}
+							endAdornment={
+								<InputAdornment sx={{ mb: "6px" }} position="end">
+									<IconButton
+										aria-label="toggle password visibility"
+										onClick={handleClickShowPassword}
+										onMouseDown={handleMouseDownPassword}
+										edge="end"
+										size="large"
+									>
+										{showPassword ? <VisibilityOff /> : <Visibility />}
+									</IconButton>
+								</InputAdornment>
+							}
+						/>
+						{/* {errors.password && (
+							<FormHelperText error id="standard-weight-helper-text-password-login">
+								{errors.password}
+							</FormHelperText>
+						)} */}
+					</FormControl>
+					<FormControl
+						fullWidth
+						// error={Boolean(errors.email)}
+						// error={true}
+						sx={{ ...theme.customInput }}
+						disabled={!matchedPrevPsw}
+					>
+						<InputLabel htmlFor="outlined-adornment-password-login">
+							New Password
+						</InputLabel>
+						<OutlinedInput
+							id="outlined-adornment-password-login"
+							// value={"values.password"}
+							name="password"
+							// onBlur={"handleBlur"}
+							// onChange={"handleChange"}
+							label="New Password"
+							type={showPassword ? "text" : "password"}
+							endAdornment={
+								<InputAdornment sx={{ mb: "6px" }} position="end">
+									<IconButton
+										disabled={!matchedPrevPsw}
+										aria-label="toggle password visibility"
+										onClick={handleClickShowPassword}
+										onMouseDown={handleMouseDownPassword}
+										edge="end"
+										size="large"
+									>
+										{showPassword ? <VisibilityOff /> : <Visibility />}
+									</IconButton>
+								</InputAdornment>
+							}
+						/>
+						{/* {errors.password && (
+							<FormHelperText error id="standard-weight-helper-text-password-login">
+								{errors.password}
+							</FormHelperText>
+						)} */}
+					</FormControl>
+					<Button
+						disableElevation
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{
+							mt: 3,
+							mb: 2,
+							p: "20px",
+							color: "white",
+							"&:hover": { backgroundColor: "primary.light" },
+						}}
+					>
+						Change Password
+					</Button>
+					<Divider sx={{ width: "100%", mb: 2 }} />
+					<Typography
+						fontWeight={500}
+						variant="body2"
+						textAlign="center"
+						color="secondary.contrastText"
+					>
+						<Link to="/login" style={{ color: "inherit" }}>
+							{"Login now!"}
+						</Link>
+					</Typography>
+				</Box>
+			</Paper>
+		</Container>
+	);
+};
+
+export default ResetPassword;
