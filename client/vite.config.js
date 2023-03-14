2e4c240c4453c7ba2684cd2c88b540e867dd756d
Minhaz <sharif35-3001@diu.edu.bd>
2023-03-14 11:21:15 +0600

    fix: build issues - client

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,11 @@ import commonjs from "@rollup/plugin-commonjs";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [commonjs(), react()],
+	build: {
+		rollupOptions: {
+			external: ["quill"],
+		},
+	},
 	devOptions: {
 		errorOverlay: true,
 	},
